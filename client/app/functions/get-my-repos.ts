2436af9f6f0dbe14ca60c17c171c67f4d6b6439e
Khushi Dhankhar <khushidhankhar006@gmail.com>
2026-04-17 22:42:11 +0530

    fix:add variable url to api

--- client/app/functions/get-my-repos.ts
+++ client/app/functions/get-my-repos.ts
@@ -11,10 +11,13 @@ export const getUserRepos = async ({
   setRepos,
 }: getUserReposPropsTypes) => {
   try {
-    const res = await axios.post("http://localhost:8080/api/get-repos", {
-      username,
-      token,
-    });
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_HOST_URL}/api/get-repos`,
+      {
+        username,
+        token,
+      },
+    );
     setRepos(res.data.repos);
   } catch {
     throw new Error("Repos not found");
